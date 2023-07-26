d9379df43d7d6d87bf299e52d524f27ae49cc15f
Raphaël Mateescu <mateescuraphael@icloud.com>
2023-07-26 21:02:41 +0200

    resized vortex values

--- Assets/effects/Permeating Cold/VortexScript.cs
+++ Assets/effects/Permeating Cold/VortexScript.cs
@@ -7,6 +7,8 @@ public class VortexScript : MonoBehaviour
     [SerializeField] List<GameObject> emitters;
     [SerializeField] List<Material> materials;
     float size;
+    public float time;
+    List<float> intensities = new List<float>();
     List<float> radiusSizes = new List<float>();
     List<float> radialForces = new List<float>();
 
@@ -21,22 +23,33 @@ public class VortexScript : MonoBehaviour
         }
         materials[0].SetFloat("_intensity", 1);
         materials[1].SetFloat("_intensity", 0.4f);
+        materials[2].SetFloat("_intensity", 0.7f);
+
+        for (int i = 0; i < materials.Count; i++)
+        {
+            intensities.Add(materials[i].GetFloat("_intensity"));
+            materials[i].SetFloat("_intensity", 0);
+        }
     }
     void Update()
     {
-        for (int i =0; i < emitters.Count; i++)
-        {
-            if (size < 2)
+        time += Time.smoothDeltaTime;
+
+        if (time >= 1.5f) 
+        { 
+            for (int i = 0; i < emitters.Count; i++)
             {
-                size += Time.deltaTime * 0.1f;
-                var sh = emitters[i].GetComponent<ParticleSystem>().shape;
-                sh.radius = radiusSizes[i] + size;
-                var vl = emitters[i].GetComponent<ParticleSystem>().velocityOverLifetime;
-                vl.radial = radialForces[i] - size;
+                materials[i].SetFloat("_intensity", materials[i].GetFloat("_intensity") - Time.deltaTime);
             }
-            else
+        }
+        else
+        {
+            for (int i = 0; i < emitters.Count; i++)
             {
-                materials[i].SetFloat("_intensity", materials[i].GetFloat("_intensity")-Time.deltaTime*0.1f);
+                if (materials[i].GetFloat("_intensity") <= intensities[i])
+                {
+                    materials[i].SetFloat("_intensity", materials[i].GetFloat("_intensity") + Time.deltaTime);
+                }
             }
         }
     }
